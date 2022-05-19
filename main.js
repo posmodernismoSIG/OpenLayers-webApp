6942d2bc2d75b3e66e862f50edfc38c7acfeb022
Joan Sebastian <posmodernismo.sig@gmail.com>
2022-05-18 21:25:27 -0500

    layer group and raster layers

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
 window.onload = init;
 function init() {
-  const fullScreenCotrol = new ol.control.FullScreen();
-  const attribution = new ol.control.Attribution();
-  const overViewMap = new ol.control.OverviewMap({
-    view: new ol.View({
-      center: [0, 0],
-      zoom: 2,
-    }),
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-      }),
-    ],
-  });
-
   const map = new ol.Map({
     view: new ol.View({
       center: [0, 0],
@@ -22,40 +8,35 @@ function init() {
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM(),
+        visible: true,
+        zIndex: 0,
+        /*extent: [
+          -8615049.674084047, 1333913.6071358265, -7640383.7940574,
+          -39401257029412733,
+        ],*/
       }),
     ],
     target: "js-map",
     keyboardEventTarget: document,
-    controls: ol.control
-      .defaults()
-      .extend([fullScreenCotrol, attribution, overViewMap]),
   });
-  const popupContainerElement = document.getElementById("popup-coordinates");
-  const popup = new ol.Overlay({
-    element: popupContainerElement,
+  const layerGroup = new ol.layer.Group({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM({
+          url: "https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+        }),
+        zIndex: 1,
+        visible: true,
+        extent: [
+          12400753.576694038, -5658730.000549673, 17174426.336716905,
+          -980228.5067132516,
+        ],
+        opacity: 1,
+      }),
+    ],
   });
-  map.addOverlay(popup);
-  map.on("click", (e) => {
-    const coordValue = e.coordinate;
-    //console.log(coordValue);
-    //popup.setPosition(undefined);
-    popup.setPosition(coordValue);
-    popupContainerElement.innerHTML = coordValue;
-  });
-  //drag rotation
-  const dragRotateInteraction = new ol.interaction.DragRotate({
-    condition: ol.events.condition.altShiftKeysOnly,
-  });
-  map.addInteraction(dragRotateInteraction);
-  //draw interaction
-  const drawInteraction = new ol.interaction.Draw({
-    type: "Polygon",
-    freehand: true,
-  });
-  map.addInteraction(drawInteraction);
-  drawInteraction.on("drawend", (x) => {
-    let parser = new ol.format.GeoJSON();
-    let drawFeatures = parser.writeFeaturesObject([x.feature]);
-    console.log(drawFeatures);
+  map.addLayer(layerGroup);
+  map.on("click", (x) => {
+    console.log(x.coordinate);
   });
 }
